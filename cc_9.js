ed80d0f13a5c8bf8e872fdd880c4147a29c3422c
Isabelalx <isabelalopesxavier@usf.edu>
2025-02-23 19:42:57 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -24,5 +24,27 @@ const emp1 = new Employee('Isabela', 123, 'IT', 50000); // Creating object emp1
 console.log(emp1.getDetails()); // Employee: Isabela
 console.log(emp1.calculateAnnualSalary()); // 600000
 
+// Task 2
 
+// Creating class Manager
+class Manager extends Employee{ //Creating class Mangager extending from Employee
+    constructor(name, id, department, salary, teamSize) { // Specified properties
+        super(name, id, department, salary); 
+        this.teamSize = teamSize;
+    };
 
+    getDetails() { 
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`; //Returning employee details
+    };
+
+    calculateBonus() { //Setting up calculateBonus
+        return super.calculateAnnualSalary() * 0.1; //Calculating bonus 
+    };
+}
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+// logging the output
+
+console.log(mgr1.calculateBonus()); 
+// Expected output: 9600
